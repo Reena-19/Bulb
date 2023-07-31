41a88d657cb0ddc94294d106a2d49163152f8c64
Reena-19 <138842205+Reena-19@users.noreply.github.com>
2023-07-31 16:45:01 +0530

    Add files via upload

---
+++ onoff.js
@@ -0,0 +1,6 @@
+function on(){
+    document.getElementById("img").src="https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-320-80.jpg"
+}
+function off(){
+    document.getElementById("img").src="https://www.freeiconspng.com/thumbs/bulb-off-icon/bulb-off-icon-14.jpg"
+}
