92ce53b8af261284bf5c3fa6ab93df8e22b457d9
Reena-19 <138842205+Reena-19@users.noreply.github.com>
2023-07-31 16:07:44 +0530

    Add files via upload

---
+++ onoff.js
@@ -0,0 +1,6 @@
+function on(){
+    document.getElementById("img").src="https://cdn.mos.cms.futurecdn.net/HaPnm6P7TZhPQGGUxtDjAg-320-80.jpg"
+}
+function off(){
+    document.getElementById("img").src="https://cdn.tutsplus.com/cdn-cgi/image/width=600/vector/uploads/legacy/tuts/149_Lightbulb_Shine/final.jpg"
+}
